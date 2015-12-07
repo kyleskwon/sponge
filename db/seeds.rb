e6a901a29fe69e299098def639065c0e375d48dc
Kyle Kwon <kyleskwon@gmail.com>
2015-12-06 19:40:01 -0800

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,6 @@
   User.create!(
     email: Faker::Internet.email,
     password: Faker::Internet.password(8, 10)
-
   )
 end
 users = User.all
@@ -11,8 +10,7 @@ users = User.all
   Wiki.create!(
     user: users.sample,
     title: Faker::Lorem.sentence,
-    body: Faker::Lorem.paragraph,
-
+    body: Faker::Lorem.paragraph
   )
 end
 
