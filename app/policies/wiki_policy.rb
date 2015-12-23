6e242f7c417686f551d82189bdaaef9d24fe0c20
Kyle Kwon <kyleskwon@gmail.com>
2015-12-23 10:47:10 -0800

    Finish edit option for collaborators

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -1,7 +1,11 @@
 class WikiPolicy < ApplicationPolicy
 
   def update?
-    user.present? && (record.user == user || user.admin? )
+    user.present? && (record.user == user || user.admin? || record.users.include?(user))
+  end
+
+  def destroy?
+    user.present? && (record.user == user || user.admin?)
   end
 
   class Scope
@@ -16,21 +20,12 @@ class WikiPolicy < ApplicationPolicy
        wikis = []
        if user && user.role == 'admin'
          wikis = scope.all # if the user is an admin, show them all the wikis
-       elsif user && user.role == 'premium'
+       else
          #wikis = scope.all
          all_wikis = scope.all
          all_wikis.each do |wiki|
            if !wiki.private? || wiki.user == user || wiki.users.include?(user)
              wikis << wiki # if the user is premium, only show them public wikis, or that private wikis they created, or private wikis they are a collaborator on
-           end
-         end
-       else # this is the lowly standard user
-         all_wikis = scope.all
-         wikis = []
-         all_wikis.each do |wiki|
-         #wikis = scope.publicly_available
-           if !wiki.private? || wiki.users.include?(user)
-             wikis << wiki # only show standard users public wikis and private wikis they are a collaborator on
            end
          end
        end
