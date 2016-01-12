a907ee93618b8241735aa80a3081c9c91f833109
Kyle Kwon <kyleskwon@gmail.com>
2016-01-12 10:51:33 -0800

    finalize Javascript snippet

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,10 +16,16 @@
 //= require bootstrap
 //= require_tree .
 var blocmetrics = {};
+blocmetrics.report = function(eventName) {
+   var event = {event: { name: eventName }};
+   var request = new XMLHttpRequest();
+   request.open("POST", "https://infinite-caverns-8137.herokuapp.com/api/events", true);
+   request.setRequestHeader('Content-Type', 'application/json');
+   request.send(JSON.stringify(event));
+}
 $( document ).ready(function() {
-  var event = {event: { name: sale }};
-  var request = new XMLHttpRequest();
-  request.open("POST", "http://localhost:3000/api/events", true);
-  request.setRequestHeader('Content-Type', 'application/json');
-  request.send(JSON.stringify(event));
+  blocmetrics.report("page load");
+$('.btn').click( function() {
+  blocmetrics.report("button clicked");
+})
 });
